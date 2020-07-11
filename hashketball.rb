1c3c6fe84b1a15c2c2b6f7cd8eb6dc3a9fe458e3
David Kirsch <davidjkirsch@gmail.com>
2020-07-11 19:06:43 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,2 +1,16 @@
 require 'pry'
 
+def game_hash
+  {
+    :home => {
+      :team_name => " ",
+      :colors => [],
+      :players => {}
+    },
+    :away => {
+      :team_name => " ",
+      :colors => [],
+      :players => {}
+    }
+  }
+end
